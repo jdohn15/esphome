42ebd70756050af227d937d900f1e1b2c12e4f3c
jdohn15 <78576051+jdohn15@users.noreply.github.com>
2025-01-03 20:21:27 -0600

    Update light.py

--- esphome/components/cwnww/light.py
+++ esphome/components/cwnww/light.py
@@ -6,39 +6,21 @@ from esphome.const import (
     CONF_COLD_WHITE,
     CONF_WARM_WHITE,
     CONF_CONSTANT_BRIGHTNESS,
-    CONF_COLD_WHITE_COLOR_TEMPERATURE,
-    CONF_WARM_WHITE_COLOR_TEMPERATURE,
     CONF_OUTPUT_ID,
+    CONF_NEUTRAL_WHITE,
 )
-
-# Define the new constants for neutral white
-CONF_NEUTRAL_WHITE = "neutral_white"
-CONF_NEUTRAL_WHITE_COLOR_TEMPERATURE = "neutral_white_color_temperature"
 
 cwnww_ns = cg.esphome_ns.namespace("cwnww")
 CWNWWLightOutput = cwnww_ns.class_("CWNWWLightOutput", light.LightOutput)
 
-CONFIG_SCHEMA = cv.All(
-    light.RGB_LIGHT_SCHEMA.extend(
-        {
-            cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(CWNWWLightOutput),
-            cv.Required(CONF_COLD_WHITE): cv.use_id(output.FloatOutput),
-            cv.Required(CONF_NEUTRAL_WHITE): cv.use_id(output.FloatOutput),
-            cv.Required(CONF_WARM_WHITE): cv.use_id(output.FloatOutput),
-            cv.Optional(CONF_COLD_WHITE_COLOR_TEMPERATURE, default=3500): cv.color_temperature,
-            cv.Optional(CONF_NEUTRAL_WHITE_COLOR_TEMPERATURE, default=1800): cv.color_temperature,
-            cv.Optional(CONF_WARM_WHITE_COLOR_TEMPERATURE, default=1000): cv.color_temperature,
-            cv.Optional(CONF_CONSTANT_BRIGHTNESS, default=False): cv.boolean,
-        }
-    ),
-    cv.has_none_or_all_keys(
-        [
-            CONF_COLD_WHITE_COLOR_TEMPERATURE,
-            CONF_NEUTRAL_WHITE_COLOR_TEMPERATURE,
-            CONF_WARM_WHITE_COLOR_TEMPERATURE,
-        ]
-    ),
-    light.validate_color_temperature_channels,
+CONFIG_SCHEMA = light.RGB_LIGHT_SCHEMA.extend(
+    {
+        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(CWNWWLightOutput),
+        cv.Required(CONF_COLD_WHITE): cv.use_id(output.FloatOutput),
+        cv.Required(CONF_NEUTRAL_WHITE): cv.use_id(output.FloatOutput),
+        cv.Required(CONF_WARM_WHITE): cv.use_id(output.FloatOutput),
+        cv.Optional(CONF_CONSTANT_BRIGHTNESS, default=False): cv.boolean,
+    }
 )
 
 
@@ -46,19 +28,17 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
     await light.register_light(var, config)
 
+    # Register cold white output
     cwhite = await cg.get_variable(config[CONF_COLD_WHITE])
     cg.add(var.set_cold_white(cwhite))
-    if cold_white_color_temperature := config.get(CONF_COLD_WHITE_COLOR_TEMPERATURE):
-        cg.add(var.set_cold_white_temperature(cold_white_color_temperature))
 
+    # Register neutral white output
     nwhite = await cg.get_variable(config[CONF_NEUTRAL_WHITE])
     cg.add(var.set_neutral_white(nwhite))
-    if neutral_white_color_temperature := config.get(CONF_NEUTRAL_WHITE_COLOR_TEMPERATURE):
-        cg.add(var.set_neutral_white_temperature(neutral_white_color_temperature))
 
+    # Register warm white output
     wwhite = await cg.get_variable(config[CONF_WARM_WHITE])
     cg.add(var.set_warm_white(wwhite))
-    if warm_white_color_temperature := config.get(CONF_WARM_WHITE_COLOR_TEMPERATURE):
-        cg.add(var.set_warm_white_temperature(warm_white_color_temperature))
 
+    # Set constant brightness if applicable
     cg.add(var.set_constant_brightness(config[CONF_CONSTANT_BRIGHTNESS]))
